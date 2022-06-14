6384eb3585e8bcb0af5e8eca3fcc309df9ccd498
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-06-14 14:39:08 +0300

    commit tree 4d2f4f397af9913d556a4d9d13590f3ec4417079

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+const findMatching = (names, str) => {
+    let myArr = [];
+    for(const name of names){
+        if(name === str || name.toLowerCase() === str.toLowerCase()){
+            myArr.push(name)
+        }
+    }
+    return myArr;
+}
+
+const fuzzyMatch = (names, str) => {
+    let myArr = [];
+    for(const name of names){
+        if(name.slice(0, 1) === str.slice(0, 1)){
+            myArr.push(name)
+        }
+    }
+    return myArr;
+}
+
+const matchName = (myArray, str) => {
+    const myArr = [];
+    for(const user of myArray){
+        if(user.name.toLowerCase() === str.toLowerCase() || user.hometown.toLowerCase() === str.toLowerCase()){
+            myArr.push(user)
+        }
+    }
+    return myArr;
+}
